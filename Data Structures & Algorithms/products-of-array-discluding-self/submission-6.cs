4a6243e0c0c4ecab7a7c3a81a192fb768ebd2478
Nabeela Hanif <nabeelahanif326@gmail.com>
2026-06-26 11:15:34 +0500

    Update: products-of-array-discluding-self - submission-6

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-6.cs
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.cs
@@ -1,19 +1,43 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
-        
-        int[] result=new int[nums.Length];
-        for(int i=0;i<nums.Length;i++)
-        {
-                int an=1;
 
-            for(int j=0;j<nums.Length;j++)
-            {
-                if(i==j) continue;
-                an*=nums[j];
-             
-            }
-               result[i]=an;
-        }
-        return result;
+    int[] ans=new int[nums.Length];
+    int[] leftPrefix=new int[nums.Length];
+    int[] rightPrefix=new int[nums.Length];
+
+     for(int i=0;i<nums.Length;i++)
+     {
+      if(i==0)
+      {
+        leftPrefix[i]=1;
+      }
+      else{
+        leftPrefix[i]=leftPrefix[i-1]*nums[i-1];
+      }
+
+     }
+    for(int j=nums.Length-1;j>=0;j--)
+    {
+     
+      if(j==nums.Length-1)
+      {
+        rightPrefix[j]=1;
+
+      }
+      else{
+        rightPrefix[j]=rightPrefix[j+1]*nums[j+1];
+      }
+     
+       
     }
-}
+    
+    
+
+    for(int i=0;i<nums.Length;i++)
+    {
+         ans[i]=leftPrefix[i]*rightPrefix[i];
+    }
+     return ans;
+
+    
+}}
