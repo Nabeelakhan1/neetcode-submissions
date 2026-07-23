34f5c292fa3ef34c9ab9330021939ca2c87a4877
Nabeela Hanif <nabeelahanif326@gmail.com>
2026-07-23 16:50:10 +0500

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        
+        for(int row=0;row<matrix.Length;row++)
+        {
+            for(int col=0;col<matrix[0].Length;col++)
+            {
+                if(matrix[row][col]==target)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
